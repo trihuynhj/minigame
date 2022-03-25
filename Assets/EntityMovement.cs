74936965ae75f99d1a201d3e91230a021c7d59a8
Tri Huynh <tri.huynh.j@gmail.com>
2022-03-26 00:22:08 +0900

    Update EntityController
    Add SpawnEntity
    Entity prefab

--- Assets/EntityMovement.cs
+++ Assets/EntityMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EntityMovement : MonoBehaviour
 {
-    [SerializeField] private Transform arenaCenter;
+    public Transform arenaCenter;
 
     private Rigidbody2D rb;
     private float speed;
@@ -15,19 +15,15 @@ public class EntityMovement : MonoBehaviour
     {
         rb = this.GetComponent<Rigidbody2D>();
         speed = .5f;
+        LinearMovement();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            LinearMovement();
-        }
-
         if (Vector3.Distance(rb.position, arenaCenter.position) < 0.1f)
         {
-            Object.Destroy(gameObject);
+            Destroy(gameObject);
         }
     }
 
--- Assets/Scripts/EntityController.cs
+++ Assets/Scripts/EntityController.cs
@@ -12,20 +12,30 @@ public class EntityController : MonoBehaviour
     public float entitySpeed;
     public float entitySpawnPosition;
 
+    [SerializeField] private GameObject entityPrefab;
+    private float respawnTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnEntity();
+        }
     }
 
     private void SpawnEntity()
     {
+        GameObject e = Instantiate(entityPrefab, transform);
+        e.transform.position = new Vector2(Random.Range(-20f, 20f), Random.Range(-20f, 20f));
 
+        EntityMovement entityMovement = e.GetComponent<EntityMovement>();
+        entityMovement.arenaCenter = transform;
     }
 }
