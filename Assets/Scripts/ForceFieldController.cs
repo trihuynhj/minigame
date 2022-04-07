d2d5502dbafd35b96a75932a77d6d38bb665f39a
Tri Huynh <tri.huynh.j@gmail.com>
2022-04-07 22:20:01 +0900

    Update ForceFieldController.cs
    Implement randomDestination in MoveToPosition method

--- Assets/Scripts/ForceFieldController.cs
+++ Assets/Scripts/ForceFieldController.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 public class ForceFieldController : MonoBehaviour
 {
+    [SerializeField] private GameController gameController;
     [SerializeField] private Transform coreTransform;
 
-    [SerializeField] private Vector3 destination;
     [SerializeField] private float circleRadius;
-    [SerializeField] private float speed;
+    [SerializeField] private float moveSpeed, linearSpeed;
 
     // Positive = counter-clockwise, negative = clockwise
     private int moveVector;
@@ -33,13 +33,13 @@ public class ForceFieldController : MonoBehaviour
         if (Input.GetKeyDown("r"))
         {
             if (linearMove) { return; }
-            StartCoroutine(MoveToPosition(circleRadius, speed, moveVector, destination));
+            StartCoroutine(MoveToPosition(circleRadius, moveSpeed, moveVector));
         }
 
         if (Input.GetKeyDown("h"))
         {
             linearMove = !linearMove;
-            if (linearMove) { StartCoroutine(LinearMovement(circleRadius, speed)); }
+            if (linearMove) { StartCoroutine(LinearMovement(circleRadius, linearSpeed)); }
         }
 ;   }
 
@@ -61,21 +61,29 @@ public class ForceFieldController : MonoBehaviour
 
             transform.position = new Vector3(x, y, 0f);
 
-            yield return null;
+            yield return new WaitForSeconds(0.001f);
         }
         
     }
 
-    private IEnumerator MoveToPosition(float _radius, float _speed, int _moveVector, Vector3 _destination)
+    private IEnumerator MoveToPosition(float _radius, float _speed, int _moveVector)
     {
+        // Generate a random destination to move to (z must be zero to check distance)
+        Vector3 xrandomDestination = coreTransform.position + Random.onUnitSphere * _radius;
+        xrandomDestination.z = 0f;
+        Debug.Log("RANDOM DESTINATION: " + xrandomDestination.ToString());
+
+        float randomRad = Random.Range(0f, 359f) * Mathf.Deg2Rad;
+        float randomX = Mathf.Cos(randomRad) * _radius + coreTransform.position.x;
+        float randomY = Mathf.Sin(randomRad) * _radius + coreTransform.position.y;
+        Vector3 randomDestination = new Vector3(randomX, randomY, 0f);
+
         // Must account for the offset of the Center Point
-        float deltaX = transform.position.x - coreTransform.position.x;
-        float deltaY = transform.position.y - coreTransform.position.y;
-
+        Vector2 delta = transform.position - coreTransform.position;
         // Calculate the current position in radian degree
-        float currentRad = Mathf.Atan2(deltaY, deltaX);
+        float currentRad = Mathf.Atan2(delta.y, delta.x);
 
-        while (Vector3.Distance(transform.position, _destination) > 0.1f)
+        while (Vector3.Distance(transform.position, randomDestination) > 1f)
         {
             currentRad += _speed * _moveVector * Time.deltaTime;
 
@@ -84,7 +92,7 @@ public class ForceFieldController : MonoBehaviour
 
             transform.position = new Vector3(_x, _y, 0f);
 
-            yield return null;
+            yield return new WaitForSeconds(0.001f);
         }
     }
 
