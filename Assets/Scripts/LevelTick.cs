2a2326ccedab25bb0afab1d0fdf96dea543ffbbb
Tri Huynh <tri.huynh.j@gmail.com>
2022-04-11 20:47:02 +0900

    Update LevelTick.cs
    Now the level tick will update in runtime according to currentLevel.
    Note that only the upward movement is implemented because the assumption is there will be no regress back to lower levels. Player loses the game once they've run out of vitalityPoint, regardless of which level they were currently at.

--- Assets/Scripts/LevelTick.cs
+++ Assets/Scripts/LevelTick.cs
@@ -19,13 +19,20 @@ public class LevelTick : MonoBehaviour
     private void Update()
     {        
         // TESTING PURPOSE
-        if (Input.GetKeyDown(KeyCode.M)) { TickMovement(); }
+        //if (Input.GetKeyDown(KeyCode.M)) { TickMovement(); }
+
+        TickMovement();
     }
 
     private void TickMovement()
     {
         float designatedYPos = traverseDistanct * gameController.currentLevel;
-        StartCoroutine(Traverse(designatedYPos));
+
+        if (coroutine_Traverse == null) 
+        {
+            coroutine_Traverse = Traverse(designatedYPos);
+            StartCoroutine(coroutine_Traverse); 
+        }
     }
 
     private IEnumerator Traverse(float _designatedYPos)
@@ -35,5 +42,9 @@ public class LevelTick : MonoBehaviour
             transform.position += new Vector3(0, traverseSpeed, 0);
             yield return null;
         }
+
+        StopCoroutine(coroutine_Traverse);
+        coroutine_Traverse = null;
     }
+    private IEnumerator coroutine_Traverse = null;
 }
